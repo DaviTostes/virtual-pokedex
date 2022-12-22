0220ec46a290dd13a55a314962fd266a1fbc3e2f
Davi Tostes <davisiqueira591@gmail.com>
2022-12-22 03:46:41 -0300

    filter por nome feito

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PokeCard from './components/PokeCard'
 
 function App() {
   const [pokedex, setPokedex] = useState([])
+  const [filter, setFilter] = useState('')
   var k=0
 
   const HandleSearch = async () => {
@@ -12,16 +13,33 @@ function App() {
     setPokedex(pokeAPI.data.results)
   }
 
+  const handleFilter = () => {
+    const cards = document.querySelectorAll('.PokeCard')
+
+    for(var i=0; i<pokedex.length; i++) {
+      if(filter != '') {
+        if(!pokedex[i].name.toLowerCase().includes(filter.toLowerCase())) {
+          cards[i].style.display = 'none'
+        } else {
+          cards[i].style.display = 'flex'
+        }
+      } else {
+        cards[i].style.display = 'flex'
+      }
+    }
+  }
+
   HandleSearch()
+  handleFilter()
 
   return (
     <div className="App">
       <div className="input-wrapper">
-        <input type="text" className='filter' id='filter' placeholder='Filter' />
+        <input type="text" className='filter' id='filter' placeholder='Filter' onChange={e => setFilter(e.target.value)} />
       </div>
       <div className="pokedex-wrapper">
         {
-          pokedex.map(poke => <PokeCard name={pokedex[k].name} 
+          pokedex.map(poke => <PokeCard className="pokeCard" name={pokedex[k].name} 
           img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${k+1}.png`}
           key={k+=1} />)
         }
