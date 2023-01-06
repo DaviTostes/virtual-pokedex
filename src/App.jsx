c39cc77b1d6e7c903ad8e4c29d1721508a694ed9
Davi Tostes <davisiqueira591@gmail.com>
2023-01-06 16:31:04 -0300

    desmanchei, fiz tudo errado, socorro

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,45 @@
 import './App.css'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { Component, useEffect, useState } from 'react'
 import PokeCard from './components/PokeCard'
+import { loadPokemons } from './utils/loadPokemons'
 
-function App() {
-  const [pokedex, setPokedex] = useState([])
-  const [filter, setFilter] = useState('')
-  var k=0
-
-  const HandleSearch = async () => {
-    const pokeAPI = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
-    setPokedex(pokeAPI.data.results)
+class App extends Component {
+  state = {
+    pokedex: []
   }
 
-  const handleFilter = () => {
-    const cards = document.querySelectorAll('.PokeCard')
+  async componentDidMount() {
+    const { pokedex } = this.state
 
-    for(var i=0; i<pokedex.length; i++) {
-      if(filter != '') {
-        if(!pokedex[i].name.toLowerCase().includes(filter.toLowerCase())) {
-          cards[i].style.display = 'none'
-        } else {
-          cards[i].style.display = 'flex'
-        }
-      } else {
-        cards[i].style.display = 'flex'
-      }
-    }
+    this.setState({ pokedex: await loadPokemons() })
   }
 
-  HandleSearch()
-  handleFilter()
+  render() {
+    const { pokedex } = this.state
 
-  return (
-    <div className="App">
-      <div className="input-wrapper">
-        <input type="text" className='filter' id='filter' placeholder='Filter' onChange={e => setFilter(e.target.value)} />
+    return (
+      <div className="App">
+        <div className="input-wrapper">
+          <input type="text" className='filter' id='filter' placeholder='Filter'/>
+        </div>
+        <div className="pokedex-wrapper">
+          {
+            pokedex.map(pokemon => {
+              return (
+                <PokeCard
+                key={pokemon.id}
+                name={pokemon.name}
+                types={pokemon.types}
+                img={pokemon.img}
+                />
+              )
+            })
+          }
+        </div>
       </div>
-      <div className="pokedex-wrapper">
-        {
-          pokedex.map(poke => <PokeCard className="pokeCard" name={pokedex[k].name} 
-          img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${k+1}.png`}
-          key={k+=1} />)
-        }
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
---
+++ src/utils/loadPokemons.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export const loadPokemons = async () => {
+  var pokemons = []
+
+  const pokeApi = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1154')
+  
+  for(let i=0; i<pokeApi.data.results.length; i++) {
+    var pokemon = await axios.get(pokeApi.data.results[i].url)
+    pokemons.push(pokemon.data)
+  }
+
+  pokemons = pokemons.map(pokemon => { 
+    return { name: pokemon.name, 
+             id: pokemon.id, 
+             types: pokemon.types.map(type => { return type.type.name }), 
+             img: pokemon.sprites.front_default }
+  })
+  return pokemons
+}
